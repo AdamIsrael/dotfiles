b46f7eb73fe4d5411bb7d171c56fab7dac8e057d
Adam Israel <adam@adamisrael.com>
2021-04-05 08:58:05 -0400

    Remove desk for OSM

--- .desk/desks/osm.sh
+++
@@ -1,9 +0,0 @@
-# osm.sh
-# Description: desk for working on Open Source Mano (OSM)
-
-cd ~/src/osm
-
-
-# Setup git to use my @canonical email
-export GIT_AUTHOR_EMAIL=adam.israel@canonical.com
-export GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL
