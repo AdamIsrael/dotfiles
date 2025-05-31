ee88c6694588ef0a570b29279e1e1766b2a21254
Adam Israel <adam@adamisrael.com>
2025-05-31 16:03:10 -0400

    Initial import of nix flake
    Giving nix a try

---
+++ flake.nix
@@ -0,0 +1,84 @@
+{
+  description = "Example nix-darwin system flake";
+
+  environment.etc.nix-darwin.source = "/Users/adam/.dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+
+      system.primaryUser = "Adam Israel";
+
+      # Disable nix-darwin's management of the Nix installation
+      # since Determinate uses its own daemon to manage Nix.
+      nix.enable = false;
+
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [
+          # Nix language server
+          pkgs.nil
+          pkgs.nixd
+
+          # Text editor
+          pkgs.neovim
+          # Terminal multiplexer
+          pkgs.tmux
+          # Git stuff
+          pkgs.git
+          pkgs.pre-commit
+        ];
+
+        homebrew = {
+          enable = true;
+          casks = [
+            # Personal Info Management
+            "obsidian"
+
+            # Browsers
+            "google-chrome"
+            "firefox"
+
+            # Password management
+            "1password"
+          ];
+        };
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      programs.zsh.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      # Add ability to used TouchID for sudo authentication
+      security.pam.services.sudo_local.touchIdAuth = true;
+
+      users.users.adam.shell = pkgs.zsh;
+      users.knownUsers = [ "adam" ];
+      users.users.adam.uid = 501;
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#tatertot
+    darwinConfigurations."tatertot" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+  };
+}
