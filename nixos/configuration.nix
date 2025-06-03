8c763010625879c6595ecd487dc772e88be2c2a8
Adam Israel <adam@adamisrael.com>
2025-06-03 19:00:17 -0400

    Install/enable 1Password + cli

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -170,6 +170,16 @@
     jq
   ];
 
+  # Enables the 1Password CLI
+  programs._1password = { enable = true; };
+
+  # Enables the 1Password desktop app
+  programs._1password-gui = {
+    enable = true;
+    # this makes system auth etc. work properly
+    polkitPolicyOwners = [ "stone" ];
+  };
+
   # Set the default editor to vim
   environment.variables.EDITOR = "nvim";
 
