2cfb4d04cdfa5e72d0d3437c424984836c33ff12
Adam Israel <adam@adamisrael.com>
2025-06-06 21:54:18 -0400

    Hyprland/waybar configuration

--- config/hypr/start.sh
+++ config/hypr/start.sh
@@ -1,11 +1,17 @@
 # Script to auto-start application(s)
-#dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
-sleep 0.3
-waybar &
-sleep 0.3
-hypridle &
-sleep 0.3
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 hyprpaper &
-# firefox &
-sleep 0.3
+# waybar occasionally times out during startup, so capture the logs for debugging.
+# add `-l debug` for verbosity
+waybar > ~/.waybar.log &
+hypridle &
+
 1password --silent &
+hyprctl dispatch exec "[workspace special:quake silent] ghostty" &
+
+# If needed, add a sleep here
+hyprctl dispatch exec "[workspace 1 silent] firefox" &
+hyprctl dispatch exec "[workspace 2 silent] zeditor" &
+# hyprctl dispatch exec "[workspace 3] discord" &
+# hyprctl dispatch exec "[workspace 4 silent] slack" &
+# hyprctl dispatch exec "[workspace 4 silent] hexchat" &
